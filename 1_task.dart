0966cd67ea5b2b6deca8eb3af3b758335bf2f7b1
boryskurit <boryskuritsyn@gmail.com>
2025-06-23 14:37:03 +0300

    ops_math

--- 1_task.dart
+++ 1_task.dart
@@ -1,3 +1,14 @@
 void main() {
-  print("Hello, World!");
+  ops_math();
+}
+
+void ops_math() {
+  var width = 18.4;
+  var height = 30.9;
+  final sq = width*height;
+  print("The area of the rectangle is: $sq");
+  final p = 2 * (width + height);
+  print ("The perimeter of the rectangle is: $p");
+  print("The ${(width ~/ 1) * (height ~/ 1)} squares sized 1х1 fit in the rectangle");
+
 }
