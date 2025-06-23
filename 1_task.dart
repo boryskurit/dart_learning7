d4ce970b4e18d0ac2293f6d0e859f3ec92377c76
boryskurit <boryskuritsyn@gmail.com>
2025-06-23 14:55:23 +0300

    void comparision

--- 1_task.dart
+++ 1_task.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 void main() {
   ops_math();
+  print('--------------------------------');
+  comparision();
 }
 
 void ops_math() {
@@ -11,4 +15,17 @@ void ops_math() {
   print ("The perimeter of the rectangle is: $p");
   print("The ${(width ~/ 1) * (height ~/ 1)} squares sized 1х1 fit in the rectangle");
 
+}
+
+void comparision () {
+  var r = Random();
+  int number = r.nextInt(300);
+  print("The number $number is ${number > 50 ? 'greater than' : 'not greater than'} 50");
+
+  if (number < 100) {
+    print("The number $number is less than 100");
+  } else {
+    print("The number $number is not less than 100");
+  }
+  print("The number $number can ${(number ~/ 5) == 0 ? '' : 'not '}be divided by 5 without a remainder");
 }
