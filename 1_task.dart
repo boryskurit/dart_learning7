e5e385ac8f71f5e9690f2954c2fb82eed23ee2ac
boryskurit <boryskuritsyn@gmail.com>
2025-06-23 15:13:39 +0300

    ternar

--- 1_task.dart
+++ 1_task.dart
@@ -9,6 +9,9 @@ void main() {
   comparision();
   print('--------------------------------');
   logical();
+  print('--------------------------------');
+  ternar();
+
 }
 
 void ops_math() {
@@ -18,27 +21,33 @@ void ops_math() {
   print("The area of the rectangle is: $sq");
   final p = 2 * (width + height);
   print ("The perimeter of the rectangle is: $p");
-  print("The ${(width ~/ 1) * (height ~/ 1)} squares sized 1х1 fit in the rectangle");
+  print("The ${(width ~/ 1) * (height ~/ 1)} squares sized 1х1 fit in the rectangle.");
 
 }
 
 void comparision () {
 
   int number = r.nextInt(300);
-  print("The number $number is ${number > 50 ? 'greater than' : 'not greater than'} 50");
+  print("The number $number is ${number > 50 ? 'greater than' : 'not greater than'} 50.");
 
   if (number < 100) {
-    print("The number $number is less than 100");
+    print("The number $number is less than 100.");
   } else {
-    print("The number $number is not less than 100");
+    print("The number $number is not less than 100.");
   }
-  print("The number $number can ${(number ~/ 5) == 0 ? '' : 'not '}be divided by 5 without a remainder");
+  print("The number $number can ${(number ~/ 5) == 0 ? '' : 'not '}be divided by 5 without a remainder.");
 }
 
 void logical() {
   bool hasMoney = r.nextBool();
   bool isStoreOpen  = r.nextBool();
 
-  print("We ${hasMoney && isStoreOpen ? 'can' : 'cannot'} buy something in the store");
-  print("We have to${!hasMoney|| !isStoreOpen ? ' not ' : ' '}wait to buy something in the store");
+  print("We ${hasMoney && isStoreOpen ? 'can' : 'cannot'} buy something in the store.");
+  print("We have to${!hasMoney|| !isStoreOpen ? ' not ' : ' '}wait to buy something in the store.");
   }
+
+void ternar() {
+  var temperature = r.nextInt(100) - 50;
+  print("The temperature is $temperature degrees Celsius, it is ${temperature > 25? 'warm enough' : 'cold enough'} outside.");
+  print("${temperature < 10? 'It is too cold outside' : ''}");
+}
